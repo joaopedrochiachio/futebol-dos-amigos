31a5416d48ae0065f7cc3c2d0d16b2043c3ac240
Joao Pedro <chiachioo04@gmail.com>
2026-03-30 00:19:43 -0300

    📚	docs | 28/03

--- dados.js
+++ dados.js
@@ -2,107 +2,107 @@ const DADOS = {
     "classificacao": [
         {
             "nome": "João",
-            "pontos": 26,
-            "jogos": 11,
-            "historico": [3, 3, 1, 3, 3, 3, 0, 1, 3, 3, "F", 3, "F"],
+            "pontos": 29,
+            "jogos": 12,
+            "historico": [3, 3, 1, 3, 3, 3, 0, 1, 3, 3, "F", 3, "F", 3],
             "gols": 20
         },
         {
             "nome": "Andrey",
             "pontos": 17,
-            "jogos": 8,
-            "historico": [3, 3, 1, 3, "F", "F", "F", 1, 3, 3, "F", 0, "F"],
+            "jogos": 9,
+            "historico": [3, 3, 1, 3, "F", "F", "F", 1, 3, 3, "F", 0, "F", 0],
             "gols": 10
         },
         {
             "nome": "Bruno",
             "pontos": 14,
-            "jogos": 12,
-            "historico": [3, 0, 1, 3, 0, 3, 0, 1, 0, 3, "F", 0, 0],
-            "gols": 9
+            "jogos": 13,
+            "historico": [3, 0, 1, 3, 0, 3, 0, 1, 0, 3, "F", 0, 0, 0],
+            "gols": 11
         },
         {
             "nome": "Manoel",
-            "pontos": 17,
-            "jogos": 10,
-            "historico": ["F", 0, 1, 3, 3, 0, "F", 1, 0, "F", 3, 3, 3],
-            "gols": 2
+            "pontos": 20,
+            "jogos": 11,
+            "historico": ["F", 0, 1, 3, 3, 0, "F", 1, 0, "F", 3, 3, 3, 3],
+            "gols": 3
         },
         {
             "nome": "Lucas Carneiro",
-            "pontos": 13,
-            "jogos": 12,
-            "historico": [3, 0, 1, 3, 0, 0, 3, 0, "F", 0, 0, 3, 0],
-            "gols": 18
+            "pontos": 16,
+            "jogos": 13,
+            "historico": [3, 0, 1, 3, 0, 0, 3, 0, "F", 0, 0, 3, 0, 3],
+            "gols": 19
         },
         {
             "nome": "Jorge",
             "pontos": 16,
-            "jogos": 10,
-            "historico": ["F", 3, 1, 0, 3, 0, "F", "F", 0, 0, 3, 3, 3],
-            "gols": 21
+            "jogos": 11,
+            "historico": ["F", 3, 1, 0, 3, 0, "F", "F", 0, 0, 3, 3, 3, 0],
+            "gols": 22
         },
         {
             "nome": "Pedro",
             "pontos": 13,
-            "jogos": 6,
-            "historico": ["F", 3, "F", "F", "F", "F", "F", 1, 3, 3, 3, 0, "F"],
+            "jogos": 7,
+            "historico": ["F", 3, "F", "F", "F", "F", "F", 1, 3, 3, 3, 0, "F", 0],
             "gols": 8
         },
         {
             "nome": "Tampico",
-            "pontos": 13,
-            "jogos": 12,
-            "historico": ["F", 3, 1, 0, 0, 3, 0, 0, 0, 0, 0, 3, 3],
-            "gols": 17
+            "pontos": 16,
+            "jogos": 13,
+            "historico": ["F", 3, 1, 0, 0, 3, 0, 0, 0, 0, 0, 3, 3, 3],
+            "gols": 19
         },
         {
             "nome": "Carlos",
             "pontos": 13,
             "jogos": 11,
-            "historico": [0, "F", 1, 0, 0, 0, 3, "F", 3, 3, 0, 0, 3],
+            "historico": [0, "F", 1, 0, 0, 0, 3, "F", 3, 3, 0, 0, 3, "F"],
             "gols": 14
         },
         {
             "nome": "Luciano",
             "pontos": 10,
-            "jogos": 11,
-            "historico": [3, 0, 1, 0, "F", 3, 3, "F", 0, 0, 0, 0, 0],
+            "jogos": 12,
+            "historico": [3, 0, 1, 0, "F", 3, 3, "F", 0, 0, 0, 0, 0, 0],
             "gols": 13
         },
         {
             "nome": "Luan",
-            "pontos": 7,
-            "jogos": 11,
-            "historico": [0, 0, 1, 0, 0, 0, 3, "F", "F", 0, 0, 3, 0],
+            "pontos": 10,
+            "jogos": 12,
+            "historico": [0, 0, 1, 0, 0, 0, 3, "F", "F", 0, 0, 3, 0, 3],
             "gols": 10
         },
         {
             "nome": "Ivair",
             "pontos": 9,
             "jogos": 8,
-            "historico": [0, 0, "F", 0, 3, "F", 0, "F", 3, 0, "F", "F", 3],
+            "historico": [0, 0, "F", 0, 3, "F", 0, "F", 3, 0, "F", "F", 3, "F"],
             "gols": 4
         },
         {
             "nome": "Maicon",
             "pontos": 1,
             "jogos": 1,
-            "historico": ["F", "F", 1, "F", "F", "F", "F", "F", "F", "F", "F", "F", "F"],
+            "historico": ["F", "F", 1, "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F"],
             "gols": 0
         },
         {
             "nome": "Edgar",
             "pontos": 0,
             "jogos": 6,
-            "historico": ["F", "F", 0, 0, 0, "F", "F", 0, 0, "F", "F", 0, "F"],
+            "historico": ["F", "F", 0, 0, 0, "F", "F", 0, 0, "F", "F", 0, "F", "F"],
             "gols": 11
         },
         {
             "nome": "Wendril",
             "pontos": 0,
             "jogos": 0,
-            "historico": ["F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F"],
+            "historico": ["F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F"],
             "gols": 0
         }
     ],
@@ -110,15 +110,15 @@ const DADOS = {
         {
             "nome": "Ivan",
             "pontos": 19,
-            "jogos": 10,
-            "historico": [3, 3, 1, 0, "F", 3, "F", "F", 0, 3, 3, 0, 0],
+            "jogos": 11,
+            "historico": [3, 3, 1, 0, "F", 3, "F", "F", 0, 3, 3, 3, 0, 0],
             "gols": 0
         },
         {
             "nome": "Flávio",
-            "pontos": 9,
-            "jogos": 8,
-            "historico": ["F", 0, "F", "F", 3, 0, "F", "F", 3, 0, 0, 0, 3],
+            "pontos": 12,
+            "jogos": 9,
+            "historico": ["F", 0, "F", "F", 3, 0, "F", "F", 3, 0, 0, 0, 3, 3],
             "gols": 0
         }
     ]
